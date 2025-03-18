73d62cfb99a30ae3773802fc7dca7c8602906068
yuml0118 <yuml0118@naver.com>
2025-03-18 19:32:46 +0900

    03.18

---
+++ web1_html_internet-master/syntax/string.js
@@ -0,0 +1,2 @@
+let data1 = 'hello. my name is lee zz';
+console.log(data1.length);
---
+++ web1_html_internet-master/syntax/template.js
@@ -0,0 +1,9 @@
+let name = 'siesta';
+let letter = 'Dear : ' + name + '\n\n Hi Hi, nice to meet you. yor are so beutiful ok ' + name + '?';
+
+let commitLetter = `Dear : ${name}
+
+Hi Hi, nice to meet you. yor are so beutiful ok ${name} ${1+2}?`;
+
+
+console.log(commitLetter);
