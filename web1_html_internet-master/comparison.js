f27c6ec661f7bec88d22fb51ef75c407ad750f2a
yuml0118 <yuml0118@naver.com>
2025-03-19 19:38:17 +0900

    03.19

---
+++ web1_html_internet-master/comparison.js
@@ -0,0 +1,6 @@
+console.log(1==1); // true / false 표현식
+console.log(1==2);
+console.log(1===1);
+console.log(1===2);
+console.log(1>2);
+console.log(1<2);
--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -1,40 +1,80 @@
-var http = require('http');
+var http = require('http'); // 웹서버 만들 때 필요한 모듈 가져오는 코드
 var fs = require('fs');
 var url = require('url');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`,'utf8', function(err, description) {
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
+    var pathname = url.parse(_url, true).pathname;
 
-      </body>
-      </html>
-      `;
-      response.end(template);
-    });
+    if(pathname === '/') {
+      if(queryData.id === undefined) {
+          fs.readdir('./data', function(err, fileList) {
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var list = '<ul>';
+            var i = 0;
+            while(i < fileList.length) {
+              list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+              i++;
+            };
+            list = list + '</ul>';
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+
+      } else {
+        fs.readdir('./data', function(err, fileList) {
+          var list = '<ul>';
+          var i = 0;
+          while(i < fileList.length) {
+            list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+            i++;
+          };
+
+          list = list + '</ul>';
+        fs.readFile(`data/${queryData.id}`,'utf8', function(err, description) {
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    };
 });
 app.listen(3000);
